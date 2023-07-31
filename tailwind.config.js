695992158542daa4023b6c3453988803435b8b2b
Abiel101 <abielortega90@gmail.com>
2023-07-31 12:20:07 -0500

    Added bara js to this project, didnt do CDN because I would have to include the links in every page and i didnt want to do that

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'text-color': '#ffffff',
+        'bg-color': '#070724',
+        'hover-state': '#38384F',
+        'medium-gray': '#838391',
+        'mercury-accent': '#419EBB',
+        'venus-accent': '#EDA249',
+        'earth-accent': '#6D2ED5',
+        'mars-accent': '#D14C32',
+        'jupiter-accent': '#D83A34',
+        'saturn-accent': '#CD5120',
+        'uranus-accent': '#1EC1A2',
+        'neptune-accent': '#2D68F0',
+      },
+      fontFamily:{
+        'antonio': 'Antonio',
+        'spartan': 'League Spartan',
+      }
+    },
   },
   plugins: [],
 }
