a84f7036921cddb1dbd717fd2ebb2c8a69dcb217
Abiel101 <abielortega90@gmail.com>
2023-09-10 08:35:15 -0500

    updated buttons (not completed yet

--- public/js/index.js
+++ public/js/index.js
@@ -10,13 +10,16 @@ const overviewBtn = document.getElementById('overview-Btn');
 const structureBtn = document.getElementById('structure-Btn');
 const surfaceBtn = document.getElementById('surface-Btn');
 
-const tabletBtns = document.querySelectorAll('contentBtns');//tablet buttons and up
+const overviewBtn_td = document.getElementById('overview-Btn-lg');
+const structureBtn_td = document.getElementById('structure-Btn-lg');
+const surfaceBtn_td = document.getElementById('surface-Btn-lg');
 
 const overviewContent = document.getElementById('overview-content');
 const structureContent = document.getElementById('structure-content');
 const surfaceContent = document.getElementById('surface-content');
 
 
+// Mobile mode for switching content
 function changeContent(content1, content2, content3, btn1, btn2, btn3){
   content1.classList.remove('hidden');
   content2.classList.add('hidden');
@@ -33,4 +36,30 @@ structureBtn.addEventListener('click', () => {
 })
 surfaceBtn.addEventListener('click', () => {
   changeContent(surfaceContent, structureContent, overviewContent, surfaceBtn, overviewBtn, structureBtn);
+})
+
+
+
+
+// tablet & desktop mode for content
+function changeContentTD(content1, content2, content3, btn1, btn2, btn3, planet){
+  content1.classList.remove('hidden');
+  content2.classList.add('hidden');
+  content3.classList.add('hidden');
+  btn1.classList.add(`active_${planet}-btn`);
+  btn2.classList.remove(`active_${planet}-btn`);
+  btn3.classList.remove(`active_${planet}-btn`);
+}
+
+overviewBtn_td.addEventListener('click', () =>{
+  // changeContentTD(overviewContent, structureContent, surfaceContent, overviewBtn_td, structureBtn_td, surfaceBtn_td, 'mercury');
+  console.log('first button clicked');
+})
+structureBtn_td.addEventListener('click', () =>{
+  // changeContentTD(structureContent, overviewContent, surfaceContent, structureBtn_td, overviewBtn_td, surfaceBtn_td, 'mercury');
+  console.log('first button clicked');
+})
+surfaceBtn_td.addEventListener('click', () =>{
+  // changeContentTD(surfaceContent, structureContent, overviewContent, surfaceBtn_td, structureBtn_td, overviewBtn_td, 'mercury');
+  console.log('first button clicked');
 })
